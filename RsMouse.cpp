aebd54cfb4c5845a8d659c2c48e75511f4c3d7d3
Billy Laws <blaws05@gmail.com>
2021-04-10 14:25:18 +0100

    Use R1/2 for left/right clicking

--- RsMouse.cpp
+++ RsMouse.cpp
@@ -136,14 +136,16 @@ Response RsMouse::FilterEvent(HidlInputEvent &iev, int32_t &deviceId) {
     if (!mRegistered)
         return Response::EVENT_DEFAULT;
 
-    // Replace R3/A/B clicks with RsMouse clicks if possible
+    // Replace R1/R2 clicks with RsMouse clicks if possible
     if (mCanClick) {
-        for (uint16_t code : {BTN_THUMBR, BTN_A, BTN_B}) {
-            if (code == iev.code) {
-                mInjector.SendKey(BTN_LEFT, iev.value);
-                mInjector.SendSynReport();
-                return Response::EVENT_SKIP;
-            }
+        if (iev.type == EV_ABS && iev.code == ABS_RZ) {
+            mInjector.SendKey(BTN_LEFT, iev.value > 0);
+            mInjector.SendSynReport();
+            return Response::EVENT_SKIP;
+        } else if (iev.type == EV_ABS && iev.code == BTN_TR) {
+            mInjector.SendKey(BTN_RIGHT, iev.value);
+            mInjector.SendSynReport();
+            return Response::EVENT_SKIP;
         }
     }
 
